09665a6241f61014eec2cc46ed3164ac0de38ee7
Kunal Vats <1kunalvats9@gmail.com>
2025-03-29 14:53:05 +0530

    final comments done

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,13 @@ const app = express();
 app.use(express.json()); 
 app.use(cors());
 
+
 app.use("/routes/auth", authRoutes);
 const MONGODB_URI = 'mongodb+srv://1kvats9:kunal19@xblog.tntxe7o.mongodb.net/?retryWrites=true&w=majority&appName=xblog'
 const JWT_SECRET = 'kunalisgood'
 
+
+//Connecting to mongodb uri
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
@@ -20,6 +23,8 @@ app.use("/routes/auth", authRoutes);
 app.get("/",(req,res)=>{
     res.send("Running the server")
 })
+
+//running the server on port 3000
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
