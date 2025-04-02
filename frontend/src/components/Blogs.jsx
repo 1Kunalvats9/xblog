fff3cfcd79ae1348a4be43b4949c30ac4e27d8c8
Kunal Vats <1kunalvats9@gmail.com>
2025-04-02 15:12:54 +0530

    link changed

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -36,12 +36,12 @@ const Blogs = () => {
   useEffect(() => {
     const fetchApi = async () => {
       let res = await fetch(
-        `https://newsapi.org/v2/everything?q=tesla&from=2025-02-28&sortBy=publishedAt&page=${page}&pageSize=${pageSize}&apiKey=3b57ffb196bb4694968511d83395c9a1`,{
+        `https://newsapi.org/v2/everything?q=tesla&from=2025-03-02&sortBy=publishedAt&apiKey=3b57ffb196bb4694968511d83395c9a1`,{
           method: "GET",
           headers: {
             "Accept": "application/json",
-            "User-Agent": "Mozilla/5.0",  // Add this line
-            "Connection": "keep-alive" // Helps prevent HTTP downgrade issues
+            "User-Agent": "Mozilla/5.0", 
+            "Connection": "keep-alive" 
           },
           keepalive: true
         });
@@ -51,8 +51,6 @@ const Blogs = () => {
     fetchApi();
   }, [page]);
 
-
-  // handling the case when user is not logged in and accessing the page.
   useEffect(() => {
     const token = localStorage.getItem("webtoken");
     if (!token) {
